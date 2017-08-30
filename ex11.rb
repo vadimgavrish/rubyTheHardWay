a6c01b7bd3dc4c5d77d9211bb1c9d859c9091791
Vadim <vadim@gavrish.ca>
2017-08-30 19:23:25 +0000

    first commit

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What car do you drive? "
+car = gets.chomp
+
+puts "Wow! So you drive a #{car}? Very cool!"
